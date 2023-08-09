632a6722e45812adc5f73765f71b73bec76a972d
Mukund-K91 <138229339+Mukund-K91@users.noreply.github.com>
2023-08-09 14:32:06 +0530

    Add files via upload

---
+++ index .js
@@ -0,0 +1,13 @@
+// var a="Thank You ! Now You Are  Continue ";
+// var b="Opps.... Something Went Wrong ):";
+
+// confirm("")
+window.addEventListener("load",function(){
+    // -------page loader--------------//
+    setTimeout(function(){
+      document.querySelector(".page-loader").style.display="none";
+  
+    },3000)
+    // ------------animation on scroll------------------//
+    AOS.init();
+  });
